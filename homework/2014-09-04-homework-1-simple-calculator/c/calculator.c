9479eaf08cb8814f12073c6e9c56608c890baf43
Ben Orchard <thefirstmuffinman@gmail.com>
2014-09-04 21:58:32 +0100

    do my homework :))) ('Homework 1', simple calc)
    took me fuckin 45 mins lol
    still, good fun getting C to do silly things. even used a case
    statement, which was a nice refresher. mostly works -- only other thing
    I'd especially like is being able to choose how many decimal places you
    want when dividing. I know where I'd place it, but I don't know how I'd
    actually use the input I get (something like an eval???).

---
+++ homework/2014-09-04-homework-1-simple-calculator/c/calculator.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int num1, num2, result;
+    float result_float;
+    int prompt_choice;
+
+    int num_options = 5;
+
+    // make sure we choose a different random seed
+    srand(time(NULL));
+
+    // pretty print intro
+    printf("*************************************\n");
+    printf("         Welcome to Calculator!\n");
+    printf("*************************************\n");
+    printf("\n");
+    printf("Operators available:\n");
+    printf("\n");
+    printf("    1) add\n");
+    printf("    2) subtract\n");
+    printf("    3) multiply\n");
+    printf("    4) divide\n");
+    printf("    5) Pick for me!\n");
+    printf("\n");
+    printf("Number of your desired operator: ");
+
+    // reading for int only to make random easier (no casting)
+    scanf("%d", &prompt_choice);
+
+    printf("\n");
+    
+    // this is unneat, but I only got 30 mins lol
+    if (prompt_choice == 5) {
+        prompt_choice = (rand() % (num_options - 1)) + 1;
+        printf("Using option %d!\n", prompt_choice);
+    }
+
+    // prompt for numbers
+    printf("Integer 1: ");
+    scanf("%d", &num1);
+
+    printf("Integer 2: ");
+    scanf("%d", &num2);
+
+    switch (prompt_choice) {
+        case 1:
+            result = num1 + num2;
+            break;
+        case 2:
+            result = num1 - num2;
+            break;
+        case 3:
+            result = num1 * num2;
+            break;
+        case 4:
+            // typecasting because C is duuuuumb
+            result_float = (float)num1 / (float)num2;
+            break;
+    }
+
+    if (prompt_choice == 4) {
+        // if we divided, we actually have to *specify* that we're talking
+        // floats -- fuckin C man ._.
+        printf("The result may or may not be %f! YMMV, no warranty. Also, you divided, and C is fucking *terrible* at that shit.\n", result_float);
+    } else {
+        printf("The result may or may not be %d! YMMV, no warranty.\n", result);
+    }
+        
+    printf("\n");
+    return 0;
+}
---
+++ homework/2014-09-04-homework-1-simple-calculator/notes.md
@@ -0,0 +1,17 @@
+https://www.edmodo.com/post/292605355
+
+Summary
+-------
+
+***Max:*** *30 mins*
+
+Write 4 separate programs:
+
+    1. add 2 input numbers & display
+    2. subtract 2 input numbers & display
+    3. multiply 2 input numbers & display
+    4. divide 2 input numbers & display
+
+Use of %d and %f in printf is mentioned.
+
+Told to write in C on [ideone.com](http://www.ideone.com).
